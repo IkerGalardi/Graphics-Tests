d45abcd8b110b4affbcb0862633d9cc37b39ed90
IkerGalardi <IkerGalardi@github.com>
2020-03-04 15:32:26 +0000

    Implemented missing function from VertexArray.cc

--- OpenGL/src/Opengl/VertexArray.cc
+++ OpenGL/src/Opengl/VertexArray.cc
@@ -4,7 +4,19 @@
 
 static int GetSizeFromGLType(unsigned int type)
 {
-
+    switch (type)
+    {
+    case GL_BYTE:               return sizeof(char);
+    case GL_UNSIGNED_BYTE:      return sizeof(unsigned char);
+    case GL_SHORT:              return sizeof(short);
+    case GL_UNSIGNED_SHORT:     return sizeof(unsigned short);
+    case GL_INT:                return sizeof(int);
+    case GL_UNSIGNED_INT:       return sizeof(unsigned int);
+    case GL_HALF_FLOAT:         return (sizeof(float) / 2);
+    case GL_FLOAT:              return sizeof(float);
+    case GL_DOUBLE:             return sizeof(double);
+    default:                    return -1;
+    }
 }
 
 namespace GL
