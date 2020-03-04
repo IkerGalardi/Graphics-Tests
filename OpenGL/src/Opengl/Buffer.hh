d76f8505b7f14833ddf25451447189ffa45e9f88
IkerGalardi <IkerGalardi@github.com>
2020-03-04 14:10:35 +0000

    Buffer abstraction added (NOT YET TESTED)

---
+++ OpenGL/src/Opengl/Buffer.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace GL
+{
+    class Buffer
+    {
+    public:
+        Buffer(unsigned int bufferType, void* buffer, unsigned int bufferSize, unsigned int drawType);
+        ~Buffer();
+
+        void Bind();
+        void Unbind();
+
+        void SetData(void* buffer, unsigned int bufferSize);
+        void SetSubData(unsigned int offset, void* buffer, unsigned int bufferSize);
+    private:
+        unsigned int BufferID;
+        unsigned int BufferType;
+        unsigned int DrawType;
+    protected:
+    };
+}
